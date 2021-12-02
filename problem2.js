7df4e50b79d1615c3134b467218bbf889e7edc94
Cory Bridewell <cory.bridewell@coxautoinc.com>
2021-12-02 15:59:14 -0500

    problem 2

---
+++ problem2.js
@@ -0,0 +1,96 @@
+const { readFile } = require("fs/promises");
+const { parse } = require("path/posix");
+
+const run1 = async () => {
+  const input = await readFile("./input2.txt", {
+    encoding: "utf-8",
+  });
+  const parseLine = (s) => {
+    const [direction, numS] = s.split(" ");
+    const num = parseInt(numS, 10);
+    if (direction === "forward") {
+      return {
+        h: num,
+        v: 0,
+      };
+    }
+    if (direction === "down") {
+      return {
+        h: 0,
+        v: num,
+      };
+    }
+    if (direction === "up") {
+      return {
+        h: 0,
+        v: num * -1,
+      };
+    }
+  };
+  const increaseCount = input
+    .split("\n")
+    .map(parseLine)
+    .reduce(
+      ({ horizontal, vertical }, { h, v }) => {
+        return {
+          horizontal: horizontal + h,
+          vertical: vertical + v,
+        };
+      },
+      { horizontal: 0, vertical: 0 }
+    );
+  console.log(increaseCount, increaseCount.horizontal * increaseCount.vertical);
+};
+
+const run2 = async () => {
+  const input = await readFile("./input2.txt", {
+    encoding: "utf-8",
+  });
+  const parseLine = (s) => {
+    const [direction, numS] = s.split(" ");
+    const num = parseInt(numS, 10);
+    if (direction === "forward") {
+      return {
+        h: num,
+        a: 0,
+      };
+    }
+    if (direction === "down") {
+      return {
+        h: 0,
+        a: num,
+      };
+    }
+    if (direction === "up") {
+      return {
+        h: 0,
+        a: num * -1,
+      };
+    }
+  };
+  const tmp = [
+    "forward 5",
+    "down 5",
+    "forward 8",
+    "up 3",
+    "down 8",
+    "forward 2",
+  ];
+  const increaseCount = input
+    .split("\n")
+    .map(parseLine)
+    .reduce(
+      ({ horizontal, vertical, aim }, { h, a }) => {
+        return {
+          aim: aim + a,
+          horizontal: horizontal + h,
+          vertical: vertical + h * aim,
+        };
+      },
+      { horizontal: 0, vertical: 0, aim: 0 }
+    );
+  console.log(increaseCount, increaseCount.horizontal * increaseCount.vertical);
+};
+
+run1();
+run2();
