1258e261db35601677ee4dc8f89bd80416bf0ec2
Cory Bridewell <cory.bridewell@coxautoinc.com>
2021-12-02 15:09:48 -0500

    problem 1

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # AdventOfCode2021
+
+## Steps to run
+
+1. Install Node.js 16.7.0+
+2. Install dependencies `npm install`
+3. Run scripts `node problem1.js`
---
+++ problem1.js
@@ -0,0 +1,52 @@
+const { readFile } = require("fs/promises");
+
+const run1 = async () => {
+  const input = await readFile("./input1.txt", {
+    encoding: "utf-8",
+  });
+  const increaseCount = input
+    .split("\n")
+    .map((x) => parseInt(x, 10))
+    .reduce(
+      ({ count, prev }, nxt) => {
+        const isIncrease = prev && nxt > prev;
+        return {
+          count: count + (isIncrease ? 1 : 0),
+          prev: nxt,
+        };
+      },
+      { count: 0, prev: 0 }
+    );
+  console.log(increaseCount);
+};
+
+const run2 = async () => {
+  const input = await readFile("./input1.txt", {
+    encoding: "utf-8",
+  });
+  const increaseCount = input
+    .split("\n")
+    // .slice(0, 10)
+    .map((x) => parseInt(x, 10))
+    .reduce(
+      ({ count, prev1, prev2, prev3 }, nxt) => {
+        const isIncrease = prev1 && prev2 && prev3 && nxt > prev1;
+        return {
+          count: count + (isIncrease ? 1 : 0),
+          prev1: prev2,
+          prev2: prev3,
+          prev3: nxt,
+        };
+      },
+      {
+        count: 0,
+        prev1: null,
+        prev2: null,
+        prev3: null,
+      }
+    );
+  console.log(increaseCount);
+};
+
+run1();
+run2();
